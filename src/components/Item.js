44801d5d5fbf633dd0216975c645c58e1189e115
JosephObote <joeobote01@gmail.com>
2023-08-20 11:22:32 +0300

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 function Item({ name, category }) {
+   const [isInCart, setIsInCart] = useState(false);
+
+  const toggleCartStatus = () => {
+    setIsInCart(prevStatus => !prevStatus);
   return (
-    <li className="">
+    <li className="in-cart">
       <span>{name}</span>
-      <span className="category">{category}</span>
+      <span className="not in cart">Remove From Cart</span>
       <button className="add">Add to Cart</button>
     </li>
   );
-}
+}}
 
 export default Item;
+
+
+
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,19 @@
 import React from "react";
 import Item from "./Item";
 
+
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const handleCategoryChange = event => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const filteredItems = selectedCategory
+    ? items.filter(item => item.category === selectedCategory)
+    : items;
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
